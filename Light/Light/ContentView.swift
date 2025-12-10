3de4cb5a277698757618537ea8fcd6d1599813d6
Aditya Bansal <adityabansalsafe@gmail.com>
2025-12-10 13:04:11 +0530

    Update project logic

--- Light/Light/ContentView.swift
+++ Light/Light/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var light: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Color.init(light ? .blue : .black)
+        
+        Button("Switch \(light ? "Off" : "On")"){
+            light = !light
         }
-        .padding()
+        
     }
 }
 
