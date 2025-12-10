cac746d0ee437bb9e2f328f04df6848d1e4588da
Aditya Bansal <adityabansalsafe@gmail.com>
2025-12-10 13:03:01 +0530

    Light project

---
+++ Light/Light/LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightApp.swift
+//  Light
+//
+//  Created by Aditya Bansal on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
